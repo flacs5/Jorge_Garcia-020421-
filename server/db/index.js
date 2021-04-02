ed52bb13d9486d4e580d43a41ac5be5152f9f0d7
Jorge Garcia <flacs5@yahoo.com>
2021-04-02 14:36:22 -0300

    change to cloud mongodb

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/bertoni-test', { useNewUrlParser: true })
+// .connect('mongodb://127.0.0.1:27017/bertoni-test', { useNewUrlParser: true })
+    .connect('mongodb+srv://flacs5:Diego2011@cluster0.s04bn.mongodb.net/bertoni-test?retryWrites=true&w=majority', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
